6d5c09add2ff978e604e1f9c2005d34efe9d56be
radubrehar <radu@jslog.com>
2014-11-25 14:42:59 +0200

    initial commit

---
+++ dist.min.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path         : __dirname + '/dist',
+        libraryTarget: 'umd',
+        library      : 'DataGrid',
+        filename     : 'react-datagrid.min.js'
+    },
+    module: {
+        loaders: require('./loaders.config')
+    },
+    externals: {
+        'react': 'React'
+    },
+    resolve: {
+        // Allow to omit extensions when requiring these files
+        extensions: ['', '.js', '.jsx']
+    }
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react')
+
+module.exports = React.createClass({
+    displayName: 'ReactDataGrid',
+
+    render: function(){
+        return <div>Hello</div>
+    }
+})
